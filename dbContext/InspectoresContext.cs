fe10f83ce0e1d1b76b95f97aa69b17e4feb34480
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2024-02-27 08:17:12 -0300

    borrado cadena de coneccion de dbContext

--- dbContext/InspectoresContext.cs
+++ dbContext/InspectoresContext.cs
@@ -19,10 +19,6 @@ public partial class InspectoresContext : DbContext
 
     public virtual DbSet<Inspectore> Inspectores { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=inspectores;Username=root;Password=root;Port=9008");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Funcione>(entity =>
